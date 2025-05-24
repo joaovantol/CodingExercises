e32077bbdd1ed5c0635faf7dff249e23a091f4de
João van Tol Guerra <joaovantol@gmail.com>
2025-05-24 13:23:13 -0300

    mine sweeper

---
+++ python/mineSweeper.py
@@ -0,0 +1,32 @@
+def mineCounter(board):
+    rows = len(board)
+    cols = len(board[0])
+    countedBoard = []
+
+    for row in range(rows):
+        countedRow = ""
+        for col in range(cols):
+            if board[row][col] == "*":
+                countedRow += "*"
+            else:
+                count = 0
+                for drow in [-1, 0, 1]:
+                    for dcol in [-1, 0, 1]:
+                        if drow == dcol == 0:
+                            continue
+                        checkrow, checkcol = row + drow, col + dcol
+                        if 0 <= checkrow < rows and 0 <= checkcol < cols:
+                            if board[checkrow][checkcol] == "*":
+                                count += 1
+                countedRow += str(count) if count > 0 else " "
+        countedBoard.append(countedRow)
+
+    return countedBoard
+
+
+board = [" * * ",
+         "  *  ",
+         "  *  ",
+         "     "]
+
+mineCounter(board)
