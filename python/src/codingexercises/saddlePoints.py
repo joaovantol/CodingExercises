8ce3b7543515cf7df247ddcf3117b5f455d3c2d6
joaovantol <joaovantol@gmail.com>
2025-06-09 17:55:38 -0300

    testes

--- python/src/codingexercises/saddlePoints.py
+++ python/src/codingexercises/saddlePoints.py
@@ -14,32 +14,3 @@ def saddlePoints(grid):
                 saddlePoints.append((rowIndex, colIndex))
     
     return saddlePoints
-
-
-
-
-saddlePoints([]) # Empty grid
-
-saddlePoints(
-    [[9, 8, 7, 8],
-     [5, 3, 2, 4, 2],
-     [6, 6, 7, 1]]
-) # Irregular grid
-
-saddlePoints(
-    [[9, 8, 7, 8],
-     [5, 3, 2, 4],
-     [6, 6, 7, 1]]
-) # [(1, 0)]
-
-saddlePoints(
-    [[9, 8, 7, 8],
-     [7, 3, 2, 4],
-     [6, 6, 7, 1]]
-) # No saddle point
-
-saddlePoints(
-    [[9, 8, 7, 8],
-     [4, 3, 2, 4],
-     [6, 6, 7, 5]]
-) # [(1, 0), (1, 3)]
--- python/tests/test_saddlePoints.py
+++ python/tests/test_saddlePoints.py
@@ -1,2 +1,36 @@
+from codingexercises.saddlePoints import saddlePoints
+
 def test_example():
-    assert True
+    assert saddlePoints(
+    [[9, 8, 7, 8],
+     [5, 3, 2, 4],
+     [6, 6, 7, 1]]) == [(1, 0)]
+
+
+
+# saddlePoints([]) # Empty grid
+
+# saddlePoints(
+#     [[9, 8, 7, 8],
+#      [5, 3, 2, 4, 2],
+#      [6, 6, 7, 1]]
+# ) # Irregular grid
+
+# saddlePoints(
+#     [[9, 8, 7, 8],
+#      [5, 3, 2, 4],
+#      [6, 6, 7, 1]]
+# ) # [(1, 0)]
+
+# saddlePoints(
+#     [[9, 8, 7, 8],
+#      [7, 3, 2, 4],
+#      [6, 6, 7, 1]]
+# ) # No saddle point
+
+# saddlePoints(
+#     [[9, 8, 7, 8],
+#      [4, 3, 2, 4],
+#      [6, 6, 7, 5]]
+# ) # [(1, 0), (1, 3)]
+
