f4407d736369f4acd2d934fed719921b089e8f47
João van Tol Guerra <joaovantol@gmail.com>
2025-06-09 11:31:09 -0300

    killerSudoku

---
+++ python/killerSudoku.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+        
+def killerSudoku(sumValue, cageSize, restrictions = []):
+    validDigits = [d for d in range(1, 10) if d not in restrictions]
+    validCombinations = [combination for combination in combinations(validDigits, cageSize) if sum(combination) == sumValue]
+
+    return validCombinations
+    
+
+killerSudoku(7, 3)
+killerSudoku(10, 2)
+killerSudoku(10, 2, [1,4])
+
