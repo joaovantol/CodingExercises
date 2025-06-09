d506403b6f3baf1f1dd5327c09e4bd34b050afa1
João van Tol Guerra <joaovantol@gmail.com>
2025-06-08 23:53:37 -0300

    saddlePoints

---
+++ python/saddlePoints.py
@@ -0,0 +1,47 @@
+def saddlePoints(grid):
+    if not grid:
+        raise ValueError("Empty grid")
+    
+    rowLengths = set([len(row) for row in grid])
+    if len(rowLengths) != 1:
+        raise ValueError("Irregular grid")
+    
+    cols = list(zip(*grid))
+    saddlePoints = set()
+    
+    for rowIndex, row in enumerate(grid):
+        for colIndex, value in enumerate(row):
+            if (value == max(row) and value == min(cols[colIndex])):
+                saddlePoints.add((rowIndex, colIndex))
+    
+    return saddlePoints
+
+grid = [
+]
+
+
+saddlePoints([]) # Empty grid
+
+saddlePoints(
+    [[9, 8, 7, 8],
+     [5, 3, 2, 4, 2],
+     [6, 6, 7, 1]]
+) # Irregular grid
+
+saddlePoints(
+    [[9, 8, 7, 8],
+     [5, 3, 2, 4],
+     [6, 6, 7, 1]]
+) # {(1, 0)}
+
+saddlePoints(
+    [[9, 8, 7, 8],
+     [7, 3, 2, 4],
+     [6, 6, 7, 1]]
+) # No good trees
+
+saddlePoints(
+    [[9, 8, 7, 8],
+     [4, 3, 2, 4],
+     [6, 6, 7, 5]]
+) # {(1, 0), (1, 3)}
