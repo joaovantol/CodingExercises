7a8b07bbda3bc060ff2b0fe4d8f5d505c6fb14c3
João van Tol Guerra <joaovantol@gmail.com>
2025-06-15 20:50:20 -0300

    sum coins

---
+++ python/src/Euler/31.py
@@ -0,0 +1,13 @@
+def coinCombinations(target):
+    coins = [1, 2, 5, 10, 20, 50, 100, 200]
+    
+    dp = [0] * (target + 1)
+    dp[0] = 1
+    
+    for coin in coins:
+        for amount in range(coin, target + 1):
+            dp[amount] += dp[amount - coin]
+    
+    return dp[amount]
+
+coinCombinations(200)
