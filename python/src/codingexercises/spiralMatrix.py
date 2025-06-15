84c0fbe295ce41b296f103f644452849eab712b9
João van Tol Guerra <joaovantol@gmail.com>
2025-06-15 19:52:17 -0300

    spiralMatrix

---
+++ python/src/codingexercises/spiralMatrix.py
@@ -0,0 +1,11 @@
+def spiralMatrix(dim):
+    matrix = [[0] * dim for _ in range(dim)]
+    row, col, drow, dcol = 0, 0, 0, 1
+
+    for matrix[row][col] in range(1, dim * dim + 1):
+        row, col= row + drow, col + dcol
+        nextRow, nextCol = row + drow, col + dcol
+        if nextRow >= dim or nextCol >= dim or nextCol < 0 or matrix[nextRow][nextCol]:
+            drow, dcol = dcol, -drow
+
+    return matrix
