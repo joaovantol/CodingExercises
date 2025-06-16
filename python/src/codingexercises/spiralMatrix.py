f79d8cff6d560dc0f55155c7adaf264b0a2672fb
joaovantol <joaovantol@gmail.com>
2025-06-16 16:49:36 -0300

    update

--- python/src/codingexercises/spiralMatrix.py
+++ python/src/codingexercises/spiralMatrix.py
@@ -3,7 +3,7 @@ def spiralMatrix(dim):
     row, col, drow, dcol = 0, 0, 0, 1
 
     for matrix[row][col] in range(1, dim * dim + 1):
-        row, col= row + drow, col + dcol
+        row, col = row + drow, col + dcol
         nextRow, nextCol = row + drow, col + dcol
         if nextRow >= dim or nextCol >= dim or nextCol < 0 or matrix[nextRow][nextCol]:
             drow, dcol = dcol, -drow
