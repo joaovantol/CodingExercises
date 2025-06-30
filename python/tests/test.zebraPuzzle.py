51e0aa36febe9789a9f221c9cc98f8aa295c91e3
joaovantol <joaovantol@gmail.com>
2025-06-30 11:28:24 -0300

    tests

--- python/tests/test.zebraPuzzle.py
+++ python/tests/test.zebraPuzzle.py
@@ -4,14 +4,14 @@ from codingexercises.zebraPuzzle import ZebraPuzzle
 def test_who_drinks_water() -> None:
     puzzle = ZebraPuzzle()
 
-    assert puzzle.drinksWater() = "norwegian"
+    assert puzzle.drinksWater() == "norwegian"
 
 def test_who_owns_zebra() -> None:
     puzzle = ZebraPuzzle()
 
-    assert puzzle.ownsZebra() = "japanese"
+    assert puzzle.ownsZebra() == "japanese"
 
 def test_solution_exists() -> None:
     puzzle = ZebraPuzzle
 
-    assert puzzle.solve() = True
+    assert puzzle.solve() == True
