0a26e935c97ec851bc738ed4390ee374b2438060
joaovantol <joaovantol@gmail.com>
2025-10-20 10:15:56 -0300

    pascal triangle

---
+++ python/src/codingexercises/pascalTriangle.py
@@ -0,0 +1,84 @@
+import math
+
+
+def expansion_pascal_row(n: int) -> list[int]:
+    """
+    Calculates the nth row of a Pascal triangle using coefficients in the
+    binomial expansion.
+
+    Args:
+        n (int): The number of row to calculate
+
+    Returns:
+        A list containing the nth row.
+    """
+    if n < 1:
+        raise ValueError("n must be a positive integer")
+
+    return [math.comb(n - 1, k) for k in range(n)]
+
+
+def iterative_pascal_row(n: int) -> list[int]:
+    """
+    Iteratively calculates the nth row of a Pascal triangle.
+
+    Args:
+        n (int): The number of row to calculate
+
+    Returns:
+        A list containing the nth row.
+    """
+    if n < 1:
+        raise ValueError("n must be a positive integer")
+
+    row = [1]
+    for _ in range(n - 1):
+        row = [x + y for x, y in zip([0] + row, row + [0])]
+
+    return row
+
+
+def recursive_pascal_row(
+    n: int,
+    row: list[int] = [1],
+) -> list[int]:
+    """
+    Recursively calculates the nth row of a Pascal triangle.
+
+    Args:
+        n (int): The number of row to calculate
+
+    Returns:
+        A list containing the nth row.
+    """
+    if n < 1:
+        raise ValueError("n must be a positive integer")
+
+    if n == 1:
+        return row
+
+    return recursive_pascal_row(
+        n - 1,
+        [x + y for x, y in zip([0] + row, row + [0])],
+    )
+
+
+def dp_pascal_row(n: int) -> list[int]:
+    """
+    Calculates the nth row of a Pascal triangle using dynamic programming.
+
+    Args:
+        n (int): The number of row to calculate
+
+    Returns:
+        A list containing the nth row.
+    """
+    if n < 1:
+        raise ValueError("n must be a positive integer")
+
+    triangle = [[1] * i for i in range(1, n + 1)]
+    for i in range(2, n):
+        for j in range(1, i):
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+    return triangle[-1]
