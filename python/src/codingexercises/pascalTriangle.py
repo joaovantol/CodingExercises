11458ac5ec46f120d34bf02323c1d616a0ec9c7d
joaovantol <joaovantol@gmail.com>
2025-11-17 13:17:54 -0300

    update pascal triangle

--- python/src/codingexercises/pascalTriangle.py
+++ python/src/codingexercises/pascalTriangle.py
@@ -2,7 +2,7 @@ import math
 from functools import reduce
 
 
-def pascal_row_bincoef(n: int) -> list[int]:
+def pascal_row_bincoef(n: int) -> list[list[int]]:
     """
     Calculates the nth row of Pascal's triangle using binomial coefficients.
 
@@ -10,15 +10,19 @@ def pascal_row_bincoef(n: int) -> list[int]:
         n (int): The number of the row to calculate
 
     Returns:
-        A list containing the nth row.
+        A list containing the Pascal triangle up to nth row.
     """
     if n < 1:
         raise ValueError("n must be a positive integer")
 
-    return [math.comb(n - 1, k) for k in range(n)]
+    triangle = [[1]]
+    for row_num in range(2, n + 1):
+        triangle.append([math.comb(row_num - 1, k) for k in range(row_num)])
+
+    return triangle
 
 
-def pascal_row_iterative(n: int) -> list[int]:
+def pascal_row_iterative(n: int) -> list[list[int]]:
     """
     Calculates the nth row of Pascal's triangle using iteration.
 
@@ -26,19 +30,19 @@ def pascal_row_iterative(n: int) -> list[int]:
         n (int): The number of the row to calculate
 
     Returns:
-        A list containing the nth row.
+        A list containing the Pascal triangle up to nth row.
     """
     if n < 1:
         raise ValueError("n must be a positive integer")
 
-    row = [1]
+    triangle = [[1]]
     for _ in range(n - 1):
-        row = [x + y for x, y in zip([0] + row, row + [0])]
+        triangle.append([x + y for x, y in zip([0] + triangle[-1], triangle[-1] + [0])])
 
-    return row
+    return triangle
 
 
-def pascal_row_reduce(n: int) -> list[int]:
+def pascal_row_reduce(n: int) -> list[list[int]]:
     """
     Calculates the nth row of Pascal's triangle using reduce and a lambda
     function.
@@ -47,22 +51,28 @@ def pascal_row_reduce(n: int) -> list[int]:
         n (int): The number of the row to calculate
 
     Returns:
-        A list containing the nth row.
+        A list containing the Pascal triangle up to nth row.
     """
     if n < 1:
         raise ValueError("n must be a positive integer")
 
-    return reduce(
-        lambda row, _: [x + y for x, y in zip([0] + row, row + [0])],
-        range(n - 1),
-        [1],
-    )
+    triangle = [[1]]
+    for row_num in range(2, n + 1):
+        triangle.append(
+            reduce(
+                lambda row, _: [x + y for x, y in zip([0] + row, row + [0])],
+                range(row_num - 1),
+                [1],
+            )
+        )
+
+    return triangle
 
 
 def pascal_row_recursive(
     n: int,
     row: list[int] = [1],
-) -> list[int]:
+) -> list[list[int]]:
     """
     Calculates the nth row of Pascal's triangle using recursion.
 
@@ -70,21 +80,30 @@ def pascal_row_recursive(
         n (int): The number of the row to calculate
 
     Returns:
-        A list containing the nth row.
+        A list containing the Pascal triangle up to nth row.
     """
     if n < 1:
         raise ValueError("n must be a positive integer")
 
     if n == 1:
-        return row
+        return [[1]]
 
-    return pascal_row_recursive(
-        n - 1,
-        [x + y for x, y in zip([0] + row, row + [0])],
-    )
+    else:
+        result = pascal_row_recursive(
+            n - 1,
+            [x + y for x, y in zip([0] + row, row + [0])],
+        )
+        last_row: list[int] = result[-1]
+        new_row: list[int] = (
+            [1]
+            + [last_row[i] + last_row[i + 1] for i in range(len(last_row) - 1)]
+            + [1]
+        )
+        result.append(new_row)
+        return result
 
 
-def pascal_row_dp(n: int) -> list[int]:
+def pascal_row_dp(n: int) -> list[list[int]]:
     """
     Calculates the nth row of Pascal's triangle using dynamic programming.
 
@@ -103,4 +122,4 @@ def pascal_row_dp(n: int) -> list[int]:
         for j in range(1, i):
             triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
 
-    return triangle[-1]
+    return triangle
--- python/tests/test_pascalTriangle.py
+++ python/tests/test_pascalTriangle.py
@@ -10,61 +10,141 @@ from codingexercises.pascalTriangle import (
 
 
 def test_bincoef_row_4() -> None:
-    expected = [1, 3, 3, 1]
+    expected = [
+        [1],
+        [1, 1],
+        [1, 2, 1],
+        [1, 3, 3, 1],
+    ]
 
     assert expected == pascal_row_bincoef(4)
 
 
 def test_iterative_row_4() -> None:
-    expected = [1, 3, 3, 1]
+    expected = [
+        [1],
+        [1, 1],
+        [1, 2, 1],
+        [1, 3, 3, 1],
+    ]
 
     assert expected == pascal_row_iterative(4)
 
 
 def test_reduce_row_4() -> None:
-    expected = [1, 3, 3, 1]
+    expected = [
+        [1],
+        [1, 1],
+        [1, 2, 1],
+        [1, 3, 3, 1],
+    ]
 
     assert expected == pascal_row_reduce(4)
 
 
 def test_recursive_row_4() -> None:
-    expected = [1, 3, 3, 1]
+    expected = [
+        [1],
+        [1, 1],
+        [1, 2, 1],
+        [1, 3, 3, 1],
+    ]
 
     assert expected == pascal_row_recursive(4)
 
 
 def test_dp_row_4() -> None:
-    expected = [1, 3, 3, 1]
+    expected = [
+        [1],
+        [1, 1],
+        [1, 2, 1],
+        [1, 3, 3, 1],
+    ]
 
     assert expected == pascal_row_dp(4)
 
 
 def test_expansion_row_10() -> None:
-    expected = [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+    expected = [
+        [1],
+        [1, 1],
+        [1, 2, 1],
+        [1, 3, 3, 1],
+        [1, 4, 6, 4, 1],
+        [1, 5, 10, 10, 5, 1],
+        [1, 6, 15, 20, 15, 6, 1],
+        [1, 7, 21, 35, 35, 21, 7, 1],
+        [1, 8, 28, 56, 70, 56, 28, 8, 1],
+        [1, 9, 36, 84, 126, 126, 84, 36, 9, 1],
+    ]
 
     assert expected == pascal_row_bincoef(10)
 
 
 def test_iterative_row_10() -> None:
-    expected = [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+    expected = [
+        [1],
+        [1, 1],
+        [1, 2, 1],
+        [1, 3, 3, 1],
+        [1, 4, 6, 4, 1],
+        [1, 5, 10, 10, 5, 1],
+        [1, 6, 15, 20, 15, 6, 1],
+        [1, 7, 21, 35, 35, 21, 7, 1],
+        [1, 8, 28, 56, 70, 56, 28, 8, 1],
+        [1, 9, 36, 84, 126, 126, 84, 36, 9, 1],
+    ]
 
     assert expected == pascal_row_iterative(10)
 
 
 def test_reduce_row_10() -> None:
-    expected = [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+    expected = [
+        [1],
+        [1, 1],
+        [1, 2, 1],
+        [1, 3, 3, 1],
+        [1, 4, 6, 4, 1],
+        [1, 5, 10, 10, 5, 1],
+        [1, 6, 15, 20, 15, 6, 1],
+        [1, 7, 21, 35, 35, 21, 7, 1],
+        [1, 8, 28, 56, 70, 56, 28, 8, 1],
+        [1, 9, 36, 84, 126, 126, 84, 36, 9, 1],
+    ]
 
     assert expected == pascal_row_reduce(10)
 
 
 def test_recursive_row_10() -> None:
-    expected = [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+    expected = [
+        [1],
+        [1, 1],
+        [1, 2, 1],
+        [1, 3, 3, 1],
+        [1, 4, 6, 4, 1],
+        [1, 5, 10, 10, 5, 1],
+        [1, 6, 15, 20, 15, 6, 1],
+        [1, 7, 21, 35, 35, 21, 7, 1],
+        [1, 8, 28, 56, 70, 56, 28, 8, 1],
+        [1, 9, 36, 84, 126, 126, 84, 36, 9, 1],
+    ]
 
     assert expected == pascal_row_recursive(10)
 
 
 def test_dp_row_10() -> None:
-    expected = [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+    expected = [
+        [1],
+        [1, 1],
+        [1, 2, 1],
+        [1, 3, 3, 1],
+        [1, 4, 6, 4, 1],
+        [1, 5, 10, 10, 5, 1],
+        [1, 6, 15, 20, 15, 6, 1],
+        [1, 7, 21, 35, 35, 21, 7, 1],
+        [1, 8, 28, 56, 70, 56, 28, 8, 1],
+        [1, 9, 36, 84, 126, 126, 84, 36, 9, 1],
+    ]
 
     assert expected == pascal_row_dp(10)
 
